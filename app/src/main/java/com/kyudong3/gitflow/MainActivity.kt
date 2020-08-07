c6c25e1ae8bba68097584d73426ec36764ce414c
kyudong3 <kyudong3@gmail.com>
2020-08-07 11:20:20 +0900

    feat: move to second activity

--- app/src/main/java/com/kyudong3/gitflow/MainActivity.kt
+++ app/src/main/java/com/kyudong3/gitflow/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.kyudong3.gitflow
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kyudong3.gitflow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setBinding()
+        moveActivity()
+    }
+
+    private fun setBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun moveActivity() {
+        binding.moveBtn.setOnClickListener {
+            startActivity(this)
+        }
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, SecondActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 }
